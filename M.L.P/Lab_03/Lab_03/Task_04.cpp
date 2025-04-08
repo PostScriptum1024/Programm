6136f7062c9f5d5ce20643a217f6314306fd5e23
Kostya <107111937+PostScriptum1024@users.noreply.github.com>
2025-04-08 22:41:43 +0500

    Add files via upload

---
+++ M.L.P/Lab_03/Lab_03/Task_04.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "Sort.h"
+
+void Task_04()
+{
+	int mas[] = { 1, 10, 3, 8, 5, 6, 7, 4, 9, 2 };
+    int size = sizeof(mas) / sizeof(mas[0]);
+
+    std::cout << "Оригинальный список:\n";
+    for (int i = 0; i < size; ++i)
+        std::cout << mas[i] << " ";
+    std::cout << std::endl;
+
+    Sort::QuickSort(mas, 0, 4);
+
+    std::cout << "Результат сортировки:\n";
+    for (int i = 0; i < size; ++i)
+        std::cout << mas[i] << " ";
+    std::cout << std::endl;
+}
